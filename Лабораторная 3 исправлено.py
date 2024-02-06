a0a57588c65f6ff4a28ec1f2ab090be2e766f183
paoloboychik <101127108+paoloboychik@users.noreply.github.com>
2024-02-06 14:36:44 +0300

    Add files via upload

---
+++ Лабораторная 3 исправлено.py
@@ -0,0 +1,61 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int):
+        if not isinstance(pages, int):
+            raise TypeError("Кол-во страниц должно быть типа int")
+        if pages < 0:
+            raise ValueError("Кол-во страниц не может быть < 0")
+        self._pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook:
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float):
+        if not isinstance(duration, float):
+            raise TypeError("Длительность должна быть типа float")
+        if duration < 0:
+            raise ValueError("Длительность не может быть отрицательным значением")
+        self._duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
